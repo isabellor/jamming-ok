6d14940b5114f51bcab4e90258b3699a2cba7069
Isabel Lorite <loriteisabel@gmail.com>
2025-08-04 19:26:07 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getAccessToken, redirectToSpotifyLogin } from "./SpotifyAuth.js";
+
 import Spotify from "./utils/Spotify.js";
 
 import SearchBar from "./components/SearchBar.jsx";
